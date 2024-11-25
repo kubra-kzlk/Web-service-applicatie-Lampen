978878416d66ab2d53b01a507fa2b1edb2fe04e9
kubra kizililic <kubra.kizilkilic@student.ap.be>
2024-11-25 12:20:03 +0100

    update

--- lampen/obj/Debug/net6.0/lampen.AssemblyInfo.cs
+++ lampen/obj/Debug/net6.0/lampen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lampen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3329c4b1aa7d46b3831ad36aeb72586598ea44ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d1e61c1d90ef94fa334485d74fb08b9d98c069")]
 [assembly: System.Reflection.AssemblyProductAttribute("lampen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lampen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
