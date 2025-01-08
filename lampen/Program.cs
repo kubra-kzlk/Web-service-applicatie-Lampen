ad3ff920d61b060b81be6b6592a7d34825a735dd
kubra kizililic <kubra.kizilkilic@student.ap.be>
2025-01-08 09:54:18 +0100

    program.cs documentation

--- lampen/Program.cs
+++ lampen/Program.cs
@@ -7,52 +7,55 @@ using Microsoft.EntityFrameworkCore;
 namespace lampen
 {
     public class Program
-    {
+    {   //DI, middleware, DB
         public static void Main(string[] args)
         {
+            //SERVICE CONFIG
             var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers(); //mog om API-endpoints aan te maken
+            builder.Services.AddEndpointsApiExplorer(); //set up Swagger for API docu API-endpoints verkennen
+            builder.Services.AddSwaggerGen(); //set up Swagger for API docu
 
-            // Retrieve the value of the InMemoryDatabase setting from appsettings.json
+            //CONDITIONAL SERVICE REGISTRATION (bplt of app in-memo db / sql db)
+            // get value of InMemoryDatabase setting from appsettings.json
             var InMemoryDatabase = builder.Configuration.GetValue<bool>("InMemoryDatabase");
-            if (InMemoryDatabase)
-            {
-                // Register in-memory implementations
-                builder.Services.AddSingleton<ILampData, InMemoryLampData>();
+            //in-memory db
+            if (InMemoryDatabase) //if true:  
+            {   //in-memo implementaties w geregis met AddSingleton (1 instantie vdeze service w gemkt en gedeeld)             
+                builder.Services.AddSingleton<ILampData, InMemoryLampData>(); //ILampData  uses InMemoryLampData
                 builder.Services.AddSingleton<IManufacturerData, ManufacturerService>();
                 builder.Services.AddSingleton<IStyleData, StyleService>();
             }
+            //SQL server db
             else
-            {    // Register SQL Server implementations using DatabaseContext
+            {    //configures Entity Framework Core to use SQL Server
+                // Register SQL Server implementations using DatabaseContext
                 builder.Services.AddDbContext<DatabaseContext>(options =>
                 {
                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-                });// Register the services to work with the actual database
+                });// Register the services to work with the actual db with AddScoped ( vr elke HTTP-aanroep w n nieuwe instantie vdeze services aangemkt)
                 builder.Services.AddScoped<ILampData, LampServiceDb>();
                 builder.Services.AddScoped<IManufacturerData, ManufacturerServiceDb>();
                 builder.Services.AddScoped<IStyleData, StyleServiceDb>();
             }
 
-            
-            
-            var app = builder.Build();
 
+            //MIDDLEWARE PIPELINE CONFIG
+            var app = builder.Build();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
+                app.UseSwagger(); //enable Swagger UI for API docu
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
+            app.UseAuthorization(); //adds authorization middleware
+            app.MapControllers(); //routing: maps attribute-routed controllers to the request pipeline
 
-            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            //LOGGING CONFIG
+            var logger = app.Services.GetRequiredService<ILogger<Program>>(); //logberichten schrijven
             if (InMemoryDatabase)
             {
                 logger.LogInformation("Using InMemory database.");
@@ -61,7 +64,8 @@ namespace lampen
             {
                 logger.LogInformation("Using SQL database.");
             }
-            app.Run();
+
+            app.Run(); //app opstarten
         }
     }
 }
